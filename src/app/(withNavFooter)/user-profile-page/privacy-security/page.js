fe381c00f37cc4e5fd2e191db226e2dbcdc9a980
Rabeya Akter <rabeyariya251@gmail.com>
2025-03-01 12:10:30 +0600

    complete design of client side

--- src/app/(withNavFooter)/user-profile-page/privacy-security/page.js
+++ src/app/(withNavFooter)/user-profile-page/privacy-security/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Radio, Switch } from "antd";
+import Link from "next/link";
 import React from "react";
 
 const PrivacyPage = () => {
@@ -11,19 +12,20 @@ const PrivacyPage = () => {
     <div className="p-5">
       <div className="border-0 border-b p-5 flex justify-between items-center mb-4">
         <div>
-          <h1 className="text-xl font-bold">Protect  Account</h1>
+          <h1 className="text-xl font-bold">Protect Account</h1>
           <p className="text-textSecondary">Change account Password</p>
         </div>
-
-        <button className="border rounded-xl px-4 py-2 text-secondary">
-        Update Password
-        </button>
+        <Link href="/user-profile-page/change-password-page">
+          <button className="border rounded-xl px-4 py-2 text-secondary">
+            Update Password
+          </button>
+        </Link>
       </div>
       <div className="border-0 border-b p-5 flex justify-between items-center mb-4">
         <div>
           <h1 className="text-xl font-bold"> Two-Factor Authentication</h1>
           <p className="text-textSecondary">
-          Add extra security by requiring a verification code when logging in.
+            Add extra security by requiring a verification code when logging in.
           </p>
         </div>
 
@@ -33,7 +35,8 @@ const PrivacyPage = () => {
         <div>
           <h1 className="text-xl font-bold">Personalized Content</h1>
           <p className="text-textSecondary">
-          Allow the app to suggest artists and guest spots based on your activity.
+            Allow the app to suggest artists and guest spots based on your
+            activity.
           </p>
         </div>
 
@@ -43,13 +46,12 @@ const PrivacyPage = () => {
         <div>
           <h1 className="text-xl font-bold">Location-Based Suggestions</h1>
           <p className="text-textSecondary">
-          Use your location to show nearby artists and events.
+            Use your location to show nearby artists and events.
           </p>
         </div>
 
         <Switch defaultChecked onChange={onChange} />
       </div>
-   
 
       <div className=" flex justify-end items-end mt-5">
         <button className="bg-primary text-white py-3 px-6 rounded-lg clear-start">
