cecc6109b000d12a43e27d329fc043b5841d50a8
Khaled Siddique <khaledssbd@gmail.com>
2025-12-26 12:49:57 +0600

    user can ask question now in /help page

--- src/services/Faq/index.ts
+++ src/services/Faq/index.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { FieldValues } from '@/types';
+import { revalidateTag } from 'next/cache';
+
 // getAllFaqs
 export const getAllFaqs = async (): Promise<any> => {
   try {
@@ -10,6 +13,29 @@ export const getAllFaqs = async (): Promise<any> => {
         tags: ['FAQS'],
       },
     });
+
+    const result = await res.json();
+    return result;
+  } catch (error: any) {
+    return Error(error);
+  }
+};
+
+// createFaqByUser
+export const createFaqByUser = async (data: FieldValues): Promise<any> => {
+  console.log({ data });
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/faq/create/by-user`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
     const result = await res.json();
     return result;
